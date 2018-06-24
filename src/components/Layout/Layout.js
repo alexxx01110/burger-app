7f8edcbbd30159fc73c7b10640f58f2fb34e98e3
Alex_m <kingkong2008@ukr.net>
2018-06-25 01:18:18 +0300

    New...

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Wrapper from "../../hoc/Wrapper";
+
+import classes from "./Layout.scss";
+
+const layout = props => (
+  <Wrapper>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Wrapper>
+);
+
+export default layout;
