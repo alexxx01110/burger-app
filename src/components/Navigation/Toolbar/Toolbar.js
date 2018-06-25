01e7af31def781fabd248a267135c5c1f02e69fa
Alex_m <kingkong2008@ukr.net>
2018-06-26 00:13:08 +0300

    Add a logo component.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import classes from "./Toolbar.scss";
+import Logo from "../../Logo/Logo";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
