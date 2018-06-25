592ecef80c23e24be3af3e4c5262bda85aca6d89
Alex_m <kingkong2008@ukr.net>
2018-06-25 16:44:53 +0300

    Create order summary component.

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
+import propTypes from "prop-types";
 
 import classes from "./BurgerIngredient.scss";
 
@@ -35,7 +35,7 @@ class BurgerIngredient extends Component {
     return ingredient;
   }
 }
-BurgerIngredient.PropTypes = {
-  type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+  type: propTypes.string.isRequired
 };
 export default BurgerIngredient;
