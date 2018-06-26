1829957e03ac7179b7f371eba3a26916aa2d67c4
Alex_m <kingkong2008@ukr.net>
2018-06-26 15:26:51 +0300

    Convert layout component to statefull, adding backdrop to sidebar.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Wrapper from "../../hoc/Wrapper";
 import classes from "./Layout.scss";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Wrapper>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Wrapper>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <Wrapper>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Wrapper>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
