510166560bc3f80f03a0161f7211ffbde87ad589
Alex_m <kingkong2008@ukr.net>
2018-06-26 11:16:57 +0300

    Implement sidbar component to layout, make logo responsible.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import Wrapper from "../../hoc/Wrapper";
 import classes from "./Layout.scss";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Wrapper>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Wrapper>
 );
